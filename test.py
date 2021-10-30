c24748b9a50a7859d74785cf00468b7ec5c708db
ahernot <70817470+ahernot@users.noreply.github.com>
2021-10-30 23:01:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,20 +9,25 @@ video3 = cv2.VideoCapture('input/RPReplay_Final1635619424.MP4')
 if not video1.isOpened():
     print('error')
 
+cv2.destroyAllWindows()
 
+i = 0
 while video1.isOpened():
     ret, frame = video1.read()
 
     if ret == True:
-        cv2.imshow('Frame', frame)
-    
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+        # cv2.imshow(f'Frame {i}', frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'): break
 
     else:
         break
+
+    i += 1
 
 video1.release()
 cv2.destroyAllWindows()
 print('done')
 
+#average all the frames together
+#then compare all the frames to this average
+#when only minimal difference in an area: add to the static part
